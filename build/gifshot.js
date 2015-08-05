e1a4c7db3577c826e432c9dd51a321af020f903b
gfranko <gregfranko5@gmail.com>
2015-08-05 10:13:23 -0700

    Release 0.2.1

--- build/gifshot.js
+++ build/gifshot.js
@@ -1667,7 +1667,7 @@ API = function (utils, error, defaultOptions, isSupported, isWebCamGIFSupported,
     'isWebCamGIFSupported': isWebCamGIFSupported,
     'isExistingVideoGIFSupported': isExistingVideoGIFSupported,
     'isExistingImagesGIFSupported': isExistingImagesGIFSupported,
-    'VERSION': '0.2.0'
+    'VERSION': '0.2.1'
   };
   return gifshot;
 }(utils, error, defaultOptions, isSupported, isWebCamGIFSupported, isExistingImagesGIFSupported, isExistingVideoGIFSupported, createGIF, takeSnapShot, stopVideoStreaming);
